83cd56a3c92203027cd7a60fd6599d97b2383999
OatsProgramming <107077453+OatsProgramming@users.noreply.github.com>
2022-08-10 01:23:42 -0500

    PingPong game completed
    Implemented:
    - game status
    - game reset
    - scoreboard

--- PongAttempt.py
+++ PongAttempt.py
@@ -2,6 +2,7 @@
 import pygame
 import os
 from sys import exit
+import time
 
 os.system('clear')
 pygame.init()
@@ -10,8 +11,8 @@ class Paddle:
     VEL = 4 
 
     def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
+        self.x = self.original_x = x
+        self.y = self.original_y = y
         self.width = width
         self.height = height
     
@@ -23,6 +24,11 @@ class Paddle:
             self.y -= self.VEL
         elif not up and self.y <= (HEIGHT - self.height): # To prevent it from going off screen (bottom)
             self.y += self.VEL
+    
+    def reset(self):
+        self.x = self.original_x
+        self.y = self.original_y
+
 
 class Ball:
     # Max Velocity for ball movement
@@ -30,8 +36,8 @@ class Ball:
     MAX_VEL = 5 
 
     def __init__(self, x, y, radius):
-        self.x = x
-        self.y = y
+        self.x = self.original_x = x  
+        self.y = self.original_y = y
         self.radius = radius
         self.x_vel = self.MAX_VEL
         self.y_vel = 0
@@ -42,17 +48,32 @@ class Ball:
     def move(self): # We'll account for the velocity's signs (+ or -) in a different function
         self.x += self.x_vel
         self.y += self.y_vel
+    
+    def reset(self):
+        self.x = self.original_x
+        self.y = self.original_y
+        self.y_vel = 0
+        self.x_vel *= -1 # This will set the ball towards the opposite direction of the score it went thru
 
 # Set up
 WIDTH, HEIGHT = 700, 500
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Ping Pong')
-left_score = 0
-right_score = 0
-text_font = pygame.font.Font('/Users/username/Documents/Python_Files/Python Lessons and notes/Pygame Tutorial/font for tutorial/Pixeltype.ttf', 50)
+END_SCORE = 3
 FPS = 60
+pygame.display.set_caption('Ping Pong')
 clock = pygame.time.Clock()
 
+# For the score and win
+left_score = 0
+right_score = 0
+win = False
+x_pos = None
+
+# Text
+text_font = pygame.font.Font('/Users/jaliljusay/Documents/Python_Files/Python Lessons and notes/Pygame Tutorial/font for tutorial/Pixeltype.ttf', 50)
+winner_text = text_font.render('You win!', False, 'White')
+restart_text = text_font.render('Press Space to restart', False, 'grey')
+restart_text = pygame.transform.scale(restart_text, (200, 25))
 
 # For ball
 RADIUS = 7
@@ -81,9 +102,9 @@ def draw(window, paddles, ball, left_score, right_score):
     # Scoreboard
                                                                                 # We divide the game board into 4 quarters for proper score placement    
     r_score = text_font.render(f'Score: {right_score}', False, 'white')
-    r_rect = r_score.get_rect(center = (WIDTH * (0.25), 100))                   # The R Score will be in the first quarter
+    r_rect = r_score.get_rect(center = (WIDTH * (0.75), 100))                   # The R Score will be in the first quarter
     l_score = text_font.render(f'Score: {left_score}', False, 'white')
-    l_rect = l_score.get_rect(center = (WIDTH * (0.75), 100))                   # The L score will be in the third
+    l_rect = l_score.get_rect(center = (WIDTH * (0.25), 100))                   # The L score will be in the third
                                                                                 # Second quarter is at mid and the Fourth will be WIDTH
     SCREEN.blit(r_score, r_rect)
     SCREEN.blit(l_score, l_rect)
@@ -103,7 +124,7 @@ def handle_movement(keys, left, right):
     if keys[pygame.K_DOWN]:
         right.move(up = False)
 
-
+# Self explanatory
 def handle_collision(ball, left, right):
     # The following will be for the collision of the ceiling
     if (ball.y + ball.radius) >= HEIGHT or (ball.y - ball.radius) <= 0:  # If just ball.y, it'll point at the center. Must get outer part of ball by adding or subtracting the radius, depending on the position
@@ -142,18 +163,49 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+        # To reset the game
+        if win and event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            left_score = 0
+            right_score = 0
+            ball.reset()
+            left_paddle.reset()
+            right_paddle.reset()
+            win = False
+        
         
     keys = pygame.key.get_pressed()
     handle_movement(keys, left_paddle, right_paddle)
     handle_collision(ball, left_paddle, right_paddle)
     ball.move()
     
-    if ball.x < 0:
-        right_score += 1
-    elif ball.x > WIDTH:
-        left_score += 1
-        
-
     draw(SCREEN, [left_paddle, right_paddle], ball, left_score, right_score)
+    
+    # Game Start
+    if not win:
+        if ball.x < 0:
+            right_score += 1
+            ball.reset()
+        elif ball.x > WIDTH:
+            left_score += 1
+            ball.reset()
+
+        if left_score > END_SCORE:
+            x_pos = 0.25
+            win = True
+        elif right_score > END_SCORE:
+            x_pos = 0.75
+            win = True
+
+    # Game Over
+    else:
+        winner_rect = winner_text.get_rect(center = (WIDTH * (x_pos), HEIGHT // 2))
+        restart_rect = restart_text.get_rect(center = (WIDTH * (x_pos), HEIGHT * (0.60)))
+        SCREEN.blit(winner_text, winner_rect)
+        SCREEN.blit(restart_text, restart_rect)
+        ball.x = WIDTH // 2
+        ball.y = HEIGHT // 2
+
+    
     pygame.display.update()
     clock.tick(FPS)
